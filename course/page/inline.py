a3b4c129a44a24c8512b60dced04fdc8219544ec
dzhuang <dzhuang.scut@gmail.com>
2015-08-19 08:26:47 +0800

    update

--- course/page/inline.py
+++ course/page/inline.py
@@ -2,7 +2,7 @@
 
 from __future__ import division
 
-__copyright__ = "Copyright (C) 2014 Andreas Kloeckner"
+__copyright__ = "Copyright (C) 2015 Andreas Kloeckner, Dong Zhuang"
 
 __license__ = """
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -129,7 +129,6 @@ class AnswerBase(object):
         self.name = name
         self.answers_desc = answers_desc
 
-        # FIXME: won't validate when required is True.
         self.required = getattr(answers_desc, "required", False)
 
     def get_correct_answer_text(self):
