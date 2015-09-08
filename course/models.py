dac08494db707fcbc09bccb8675343f5dd94ae96
dzhuang <dzhuang.scut@gmail.com>
2015-09-08 14:48:53 +0800

    better grade display.

--- course/models.py
+++ course/models.py
@@ -545,8 +545,13 @@ class FlowSession(models.Model):
                 .order_by("-visit_time")
                 [:1]):
             return visit.visit_time
-
         return None
+
+    def get_expiration_mode_desc(self):
+        return dict(FLOW_SESSION_EXPIRATION_MODE_CHOICES).get(
+            self.expiration_mode)
+
+        
 
 # }}}
 
@@ -1034,6 +1039,10 @@ class GradingOpportunity(models.Model):
                     "opportunity_id": self.identifier,
                     "course": self.course})
 
+    def get_aggregation_strategy_descr(self):
+        return dict(GRADE_AGGREGATION_STRATEGY_CHOICES).get(
+                    self.aggregation_strategy)
+
 
 class GradeChange(models.Model):
     """Per 'grading opportunity', each participant may accumulate multiple grades
@@ -1107,6 +1116,10 @@ class GradeChange(models.Model):
             return 100*self.points/self.max_points
         else:
             return None
+
+    def get_state_desc(self):
+        return dict(GRADE_STATE_CHANGE_CHOICES).get(
+            self.state)
 
 # }}}
 
