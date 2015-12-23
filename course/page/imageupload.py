f701d36b27ce198a38d9b200b1a553fbcec70708
dzhuang <dzhuang.scut@gmail.com>
2015-12-23 19:36:04 +0800

    update

--- course/page/imageupload.py
+++ course/page/imageupload.py
@@ -164,7 +164,6 @@ class ImageUploadForm(StyledForm):
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                 <h4 class="modal-title">Modal title</h4>
 
                             </div>
                             <div class="modal-body"><div class="te"></div></div>
