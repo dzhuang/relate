1c6d344677967c6beeb617f32bc5069b78ee352b
dzhuang <dzhuang.scut@gmail.com>
2016-03-27 18:45:04 +0800

    remove announcement.

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'RELATE'
-copyright = u'2014, Andreas Kloeckner'
+copyright = u'2015, Andreas Kloeckner, Dong Zhuang'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
