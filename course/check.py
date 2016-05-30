402a874240d9761ff48839ce55709447202ee602
dzhuang <dzhuang.scut@gmail.com>
2016-05-30 18:52:30 +0800

    reorganized.

--- course/check.py
+++ course/check.py
@@ -7,11 +7,12 @@ from django.core.management.base import CommandError
 from django.utils.encoding import (
     DEFAULT_LOCALE_ENCODING, force_text)
 import re
-
+import os
 
 class Tags(DjangoTags):
     relate_course_tag = 'relate_course_tag'
 
+@register(Tags.relate_course_tag)
 @register(Tags.relate_course_tag, deploy=True)
 def latex2image_bin_check(app_configs, **kwargs):
     if not getattr(settings, "RELATE_LATEX_TO_IMAGE_ENABLED", False):
@@ -22,21 +23,28 @@ def latex2image_bin_check(app_configs, **kwargs):
     imagemagick_bin_path = getattr(settings, "RELATE_IMAGEMAGICK_BIN_PATH", "")
     latex_bin_path = getattr(settings, "RELATE_LATEX_BIN_PATH", "")
 
-    RELATE_LATEX_TO_IMAGE_ENV = prepend_bin_path_to_subprocess_env([imagemagick_bin_path, latex_bin_path])
+    #RELATE_LATEX_TO_IMAGE_ENV = prepend_bin_path_to_subprocess_env([imagemagick_bin_path, latex_bin_path])
 
     for cmd in CMD_NAME_DICT:
 
         # ImageMagick need shell=True
-        enable_shell = False
-        if cmd == "convert":
-            enable_shell = True
+        # enable_shell = False
+        # if cmd == "convert":
+        #     enable_shell = True
 
         out = ""
         strerror = ""
+        cmd_with_path = cmd
+        if cmd == "convert":
+            if imagemagick_bin_path:
+                cmd_with_path = os.path.join(imagemagick_bin_path, cmd)
+        else:
+            if latex_bin_path:
+                cmd_with_path = os.path.join(latex_bin_path, cmd)
         try:
-            out, err, status = popen_wrapper([cmd, '--version'], stdout_encoding=DEFAULT_LOCALE_ENCODING,
-                                             shell=enable_shell,
-                                             env=RELATE_LATEX_TO_IMAGE_ENV)
+            out, err, status = popen_wrapper([cmd_with_path, '--version'], stdout_encoding=DEFAULT_LOCALE_ENCODING)
+        # except KeyError as e:
+        #     strerror = str(e)
         except CommandError as e:
             strerror = e.__str__()
 
@@ -48,7 +56,7 @@ def latex2image_bin_check(app_configs, **kwargs):
                     hint=("Unable to run %(cmd)s. Is %(tool)s installed "
                           "or has its path correctly configured "
                           "in local_settings.py?")
-                         % {"cmd": cmd,
+                         % {"cmd": cmd_with_path,
                             "tool": CMD_NAME_DICT[cmd],
                             },
                     obj=CMD_NAME_DICT[cmd]
