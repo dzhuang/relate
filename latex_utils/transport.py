90dd7e3cc9f0463b3dd2ed85957fc12a412011e0
dzhuang <dzhuang.scut@gmail.com>
2016-06-11 19:32:28 +0800

    update transport.py, 进行到nonzeros.

--- latex_utils/transport.py
+++ latex_utils/transport.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 from collections import Counter
 
@@ -14,31 +16,72 @@ def transport(supply, demand, costs):
     n, m = C.shape
 
     # Finding initial solution
-    X = np.zeros((n, m))
+    #X = np.zeros((n, m))
+    X = np.full((n, m), np.nan)
+    allow_fill_X = np.ones((n, m), dtype=bool)
     indices = [(i, j) for i in range(n) for j in range(m)]
+
+    # 最小元素法
     xs = sorted(zip(indices, C.flatten()), key=lambda (a, b): b)
+
+    # 西北角法
+    #xs = sorted(zip(indices, C.flatten()), key=lambda (a, b): (a[0],a[1]))
+
+    #print xs
 
     # Iterating C elements in increasing order
     for (i, j), _ in xs:
-        if d[j] == 0:
+        # if d[j] == 0:
+        #     continue
+        # else:
+        #     # Reserving supplies in a greedy way
+        #     #print min([s[i], d[j]])
+        #     remains = s[i] - d[j] if s[i] >= d[j] else 0
+        #     grabbed = s[i] - remains
+        #     X[i, j] = grabbed
+        #     s[i] = remains
+        #     d[j] -= grabbed
+        grabbed = min([s[i],d[j]])
+        #print grabbed
+        if grabbed == 0:
+            continue
+        elif not np.isnan(X[i,j]):
+            # X[i,j]单元格未填数字
             continue
         else:
-            # Reserving supplies in a greedy way
-            remains = s[i] - d[j] if s[i] >= d[j] else 0
-            grabbed = s[i] - remains
+            # 行与列同时满足，退化（除非是最后一个单元格）
             X[i, j] = grabbed
-            s[i] = remains
+            if s[i] == grabbed and d[j] == grabbed:
+                allow_fill_X[i, j] = False
+                allowed_indices = [(ii, jj) for ii in range(n) for jj in range(m) if allow_fill_X[ii,jj]]
+                if allowed_indices:
+                    # 在允许填写数字的单元格补0
+                    X[allowed_indices[0]] = 0
+                    allow_fill_X[allowed_indices[0]] = False
+            s[i] -= grabbed
             d[j] -= grabbed
+        #print s[i], d[j]
 
-    #print X
+        if d[j] == 0:
+            allow_fill_X[:,j] = False
+        if s[i] == 0:
+            allow_fill_X[i,:] = False
+
+        #print allow_fill_X
+
+
+
+    print 'solution is', X
     # Finding optimal solution
     while True:
         u = np.array([np.nan]*n)
         v = np.array([np.nan]*m)
         S = np.zeros((n, m))
 
-        _x, _y = np.where(X > 0)
+        #_x, _y = np.where(X > 0)
+        _x, _y = np.where(~np.isnan(X))
         nonzero = zip(_x, _y)
+        print nonzero
         f = nonzero[0][0]
         u[f] = 0
 
@@ -57,11 +100,12 @@ def transport(supply, demand, costs):
             for j in range(m):
                 S[i, j] = C[i, j] - u[i] - v[j]
 
-        print S
+        print 's-matrix is ', S
 
         # Stop condition
         s = np.min(S)
         if s >= 0:
+            #print 'Final solution is', X
             break
 
         i, j = np.argwhere(S == s)[0]
@@ -69,7 +113,7 @@ def transport(supply, demand, costs):
 
         # Finding cycle elements
         T = np.copy(X)
-        T[start] = 1  
+        T[start] = 1
         while True:
             _xs, _ys = np.nonzero(T)
             xcount, ycount = Counter(_xs), Counter(_ys)
@@ -105,19 +149,22 @@ def transport(supply, demand, costs):
         q = min(X[zip(*neg)])
         X[zip(*neg)] -= q
         X[zip(*pos)] += q
+        #print 'solution is', X
 
     return X, np.sum(X*C)
 
 
 if __name__ == '__main__':
-    supply = np.array([200, 350, 300])
+    supply = np.array([110, 350, 390])
     demand = np.array([270, 130, 190, 150, 110])
+
+    #from sympy import *
 
     costs = np.array([[24., 50., 55., 27., 16.],
                       [50., 40., 23., 17., 21.],
                       [35., 59., 55., 27., 41.]])
 
     routes, z = transport(supply, demand, costs)
-    assert z == 23540
+    #assert z == 23540
 
-    print routes
+    #print routes
