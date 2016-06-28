1fa83c6e5cf6c6b107ee87abf0095207488c39ca
dzhuang <dzhuang.scut@gmail.com>
2016-06-29 00:00:14 +0800

    add repository of docker to dzhuang/learning-what-runpy-i386

--- docker-image-run-py/docker-build.sh
+++ docker-image-run-py/docker-build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 cp ../course/page/code_feedback.py ../course/page/code_runpy_backend.py .
-docker build .
+docker build -t "dzhuang/learning-what-runpy-i386" .
 rm code_feedback.py code_runpy_backend.py
 
