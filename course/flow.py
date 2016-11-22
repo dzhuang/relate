b18dda8d0cd5eb68e327982f1ef720db3c9214ad
dzhuang <dzhuang.scut@gmail.com>
2016-11-22 18:51:14 +0800

    only half done.

--- course/flow.py
+++ course/flow.py
@@ -1586,10 +1586,11 @@ def get_page_behavior(
                 flow_permission.see_answer_after_submission in permissions)
 
     if session_in_progress:
+        print permissions
         show_answer = (
             show_answer
             and
-            flow_permission.cannot_see_answer_for_sessions_in_progress in permissions)
+            flow_permission.cannot_see_answer_for_sessions_in_progress not in permissions)
 
     may_change_answer = (
             not viewing_prior_version
