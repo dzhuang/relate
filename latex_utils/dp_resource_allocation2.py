dce0afbc2881f8be1c9c9737e0beac4f251a1070
dzhuang <dzhuang.scut@gmail.com>
2016-11-24 02:58:00 +0800

    update.

--- latex_utils/dp_resource_allocation2.py
+++ latex_utils/dp_resource_allocation2.py
@@ -1380,4 +1380,4 @@ for i, dp_dict in enumerate(dp_list_loaded):
         dp=dp,
         dp_result_list=result_list,
     )
-    r.clipboard(question_tex + solve_tex)
+    r.clipboard_append(question_tex + solve_tex)
