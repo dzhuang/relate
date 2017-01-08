e8a1ab11687775e2ddd6ed786d5ffd6cc28d2f74
dzhuang <dzhuang.scut@gmail.com>
2017-01-08 21:30:42 +0800

    update.

--- course_statistics/views.py
+++ course_statistics/views.py
@@ -1,12 +1,33 @@
+
+from django.utils.translation import (
+        ugettext_lazy as _, pgettext_lazy, ugettext, string_concat)
 from django.shortcuts import render
 
-from course.utils import course_view
+from django.core.exceptions import (
+        PermissionDenied, SuspiciousOperation, ObjectDoesNotExist)
+
+from course.utils import (
+        course_view, render_course_page,
+        get_session_access_rule)
+from course.constants import (
+        participation_permission as pperm,
+        flow_permission
+        )
 # Create your views here.
 
 @course_view
 def view_stat_book(pctx):
-    pass
+    request = pctx.request
+    if not pctx.has_permission(pperm.view_gradebook):
+        raise PermissionDenied(_("may not view statistics"))
+    return render_course_page(pctx, "course_statistics/statistic_book.html",
+                              {})
+
 
 @course_view
 def view_stat_by_question(pctx, question_id):
-    pass
+    request = pctx.request
+    if not pctx.has_permission(pperm.view_gradebook):
+        raise PermissionDenied(_("may not view statistics"))
+    return render_course_page(pctx, "course_statistics/statistic_book.html",
+                              {})
