2e850bae4ae88899b1aa2f6ff2487c1ad4f0358b
dzhuang <dzhuang.scut@gmail.com>
2017-03-07 02:49:52 +0800

    update for qiniu deploy static

--- relate/settings.py
+++ relate/settings.py
@@ -58,15 +58,11 @@ INSTALLED_APPS = (
 
     "accounts",
     "course",
-)
+    ) + local_settings.get("RELATE_CUSTOM_INSTALLED_APPS", ())
 
-if local_settings.get("RELATE_CUSTOM_INSTALLED_APPS", False):  # type: ignore
-    INSTALLED_APPS = INSTALLED_APPS + local_settings["RELATE_CUSTOM_INSTALLED_APPS"]  # type: ignore # noqa
+INSTALLED_APPS = INSTALLED_APPS + ("django.contrib.staticfiles",)
 
-if not local_settings.get("RELATE_STATIC_CDN_ENABLED", False):  # type: ignore
-    INSTALLED_APPS = INSTALLED_APPS + ("django.contrib.staticfiles",)  # type: ignore # noqa
-
-if local_settings.get("RELATE_SIGN_IN_BY_SAML2_ENABLED", False):  # type: ignore
+if local_settings.get("RELATE_SIGN_IN_BY_SAML2_ENABLED", False):
     INSTALLED_APPS = INSTALLED_APPS + ("djangosaml2",)  # type: ignore
 
 # }}}
@@ -116,7 +112,7 @@ AUTH_USER_MODEL = 'accounts.User'
 BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "components")
 
 STATICFILES_FINDERS = tuple(STATICFILES_FINDERS) + (
-    "djangobower.finders.BowerFinder",
+    #"djangobower.finders.BowerFinder",
     )
 
 
