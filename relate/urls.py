22ad70c9120b7a42988b4beeff04c562d4fcca14
dzhuang <dzhuang.scut@gmail.com>
2017-06-07 17:31:45 +0800

    debug_toolbar

--- relate/urls.py
+++ relate/urls.py
@@ -552,10 +552,10 @@ from django.conf.urls.static import static
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns += [
-#         url(r'^__debug__/', include(debug_toolbar.urls)),
-#     ]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
 
 # vim: fdm=marker
