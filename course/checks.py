830e4474c9c8684f752e2f166b9e0acb2969efaa
dzhuang <dzhuang.scut@gmail.com>
2017-09-12 23:19:35 +0800

    Extending runpy docker configuration / enable docker-machine

--- course/checks.py
+++ course/checks.py
@@ -45,15 +45,23 @@ GIT_ROOT = "GIT_ROOT"
 RELATE_STARTUP_CHECKS = "RELATE_STARTUP_CHECKS"
 RELATE_STARTUP_CHECKS_EXTRA = "RELATE_STARTUP_CHECKS_EXTRA"
 
-RELATE_STARTUP_CHECKS_TAG = "start_up_check"
+RELATE_STARTUP_CHECKS_TAG = "start_up_checks"
 RELATE_STARTUP_CHECKS_EXTRA_TAG = "startup_checks_extra"
+
+# {{{ for mypy
+
+if False:
+    from typing import Any  # noqa
+
+# }}}
 
 
 class RelateCriticalCheckMessage(Critical):
     def __init__(self, *args, **kwargs):
+        # type: (*Any, **Any) -> None
         super(RelateCriticalCheckMessage, self).__init__(*args, **kwargs)
-        if not self.obj:
-            self.obj = ImproperlyConfigured.__name__
+        if not self.obj:  # type: ignore
+            self.obj = ImproperlyConfigured  # type: Any
 
 
 class DeprecatedException(Exception):
