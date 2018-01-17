34e71a3a0d94c4305efcfe658f8e3a5b98413453
dzhuang <dzhuang.scut@gmail.com>
2018-01-17 14:21:07 +0800

    update questionnaire admin

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import copy
 from django.contrib import admin
-
-from .models import (Questionnaire, Question,
-                     Choice, Answer)
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import messages
+from .models import (Questionnaire, Question, Choice, Answer)
 
 
 class AnswerAdmin(admin.ModelAdmin):
@@ -11,6 +12,7 @@ class AnswerAdmin(admin.ModelAdmin):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ('questionnaire',)
     list_display = (
             "id",
             "text",
@@ -21,11 +23,42 @@ class QuestionAdmin(admin.ModelAdmin):
             )
 
 
+def duplicate_questionnaire(modeladmin, request, queryset):
+
+    if queryset.count() > 1:
+        messages.add_message(
+            request, messages.ERROR,
+            _("Only one questionnaire is allowed to be copied per request"))
+        return
+
+    questionnare = queryset[0]
+
+    q_copy = copy.copy(questionnare)
+    q_copy.id = None
+    q_copy.save()
+    for question in questionnare.questions():
+        question_copy = copy.copy(question)
+        question_copy.id = None
+        question_copy.questionnaire = q_copy
+        question_copy.save()
+
+        choice_set = Choice.objects.filter(question=question).order_by("pk")
+        for choice in choice_set:
+            choice_copy = copy.copy(choice)
+            choice_copy.id = None
+            choice_copy.question = question_copy
+            choice_copy.save()
+
+duplicate_questionnaire.shoort_description = _("Make a copy of this questionnaire")
+
+
 class QuestionnaireAdmin(admin.ModelAdmin):
     list_display = (
             "id",
             "__str__",
             )
+    actions = [duplicate_questionnaire]
+    list_filter = ("questionnaire", "questionnaire__course")
 
 
 admin.site.register(Questionnaire, QuestionnaireAdmin)
--- survey/admin.py
+++ survey/admin.py
@@ -9,6 +9,7 @@ from copy import deepcopy
 
 
 class ParticipationSurveyAdmin(admin.ModelAdmin):
+    list_filter = ('course',)
     save_as = True
     list_display = (
         "id",
@@ -18,23 +19,3 @@ class ParticipationSurveyAdmin(admin.ModelAdmin):
 
 admin.site.register(CourseSurvey, ParticipationSurveyAdmin)
 admin.site.register(ParticipationSurveyQuestionAnswer)
-
-
-def duplicate(questionnaire_id):
-    questionnare = Questionnaire.objects.get(id=questionnaire_id)
-
-    q_copy = deepcopy(questionnare)
-    q_copy.id = None
-    q_copy.save()
-    for question in questionnare.questions():
-        question_copy = deepcopy(question)
-        question_copy.id = None
-        question_copy.questionnaire = q_copy
-        question_copy.save()
-
-        choice_set = Choice.objects.filter(question=question).order_by("pk")
-        for choice in choice_set:
-            choice_copy = deepcopy(choice)
-            choice_copy.id = None
-            choice_copy.question = question_copy
-            choice_copy.save()
