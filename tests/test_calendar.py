0a2258f1fe98640a95a0c6e6da8d34eb684cf7f9
dzhuang <dzhuang.scut@gmail.com>
2018-04-24 23:00:35 +0800

    update test_calendar.py

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -819,30 +819,6 @@ class ViewCalendarTest(SingleCourseTestMixin, HackRepoMixin, TestCase):
              'end': lecture2_end_time.isoformat()
              })
 
-# vim: fdm=marker
-from __future__ import division
-
-__copyright__ = "Copyright (C) 2017 Dong Zhuang"
-
-__license__ = """
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
 
 import json
 from django.test import TestCase
@@ -855,7 +831,6 @@ from tests.base_test_mixins import (
     SingleCourseTestMixin, FallBackStorageMessageTestMixin)
 from tests.utils import mock
 
-DATE_TIME_PICKER_TIME_FORMAT = "%Y-%m-%d %H:%M"
 
 SHOWN_EVENT_KIND = "test_open_event"
 HIDDEN_EVENT_KIND = "test_secret_event"
