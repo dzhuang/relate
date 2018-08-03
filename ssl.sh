3c767c5f01331e064fa64c20db86aa0ca36f1937
dzhuang <dzhuang.scut@gmail.com>
2018-08-03 14:10:33 +0800

    prevent too many certificate for learningwhat.

--- ssl.sh
+++ ssl.sh
@@ -3,6 +3,6 @@
 # this script needs to be added to crontab
 # 0 5 1 * * /home/course/relate/ssl.sh
 systemctl stop nginx
-docker  exec  acme.sh --issue -d www.learningwhat.com  --standalone --force
+docker  exec  acme.sh --issue -d www.learningwhat.com  --standalone
 systemctl start nginx
 nginx -s reload
