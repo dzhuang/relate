57b96226858dec8e0cf0856915edacee02d8b6bb
dzhuang <dzhuang.scut@gmail.com>
2018-08-03 14:04:55 +0800

    Add crontab to run acme.sh

--- ssl.sh
+++ ssl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # this script needs to be added to crontab
+# 0 5 1 * * /home/course/relate/ssl.sh
 systemctl stop nginx
 docker  exec  acme.sh --issue -d www.learningwhat.com  --standalone --force
 systemctl start nginx
