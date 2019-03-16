964c5360c040f929657f2726e635bbf2bfe7dd84
dzhuang <dzhuang.scut@gmail.com>
2019-03-16 18:19:08 +0800

    Run docker update certificate.

--- ssl.sh
+++ ssl.sh
@@ -2,6 +2,8 @@
 
 # this script needs to be added to crontab
 # 0 5 1 * * /home/course/relate/ssl.sh
+docker rm /acme.sh --force
+docker run --rm  -itd    -v "/etc/nginx/cert":/acme.sh    --net=host   --entrypoint /bin/sh   --name=acme.sh   neilpang/acme.sh
 systemctl stop nginx
 docker  exec  acme.sh --issue -d www.learningwhat.com  --standalone
 systemctl start nginx
